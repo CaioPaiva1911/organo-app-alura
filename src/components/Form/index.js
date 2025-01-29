ed4d9e32d2d3f14e4be22fb4963fd35cfab3658d
Caio Paiva <caiowick@hotmail.com>
2025-01-29 20:36:01 -0300

    creating button and dropdown list

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,21 @@
+import Button from '../Button'
+import DropdownList from '../DropdownList'
 import TextInput from '../TextInput'
 import './Form.css'
 
+
 const Form = () => {
+
+    const teams = [
+        "Programming",
+        "Front-End",
+        "Data Science",
+        "DevOps",
+        "UX and Design",
+        "Mobile",
+        "Inovation and Business"
+    ]
+
     return (
         <section className="form">
             <form>
@@ -9,6 +23,10 @@ const Form = () => {
                 <TextInput label="Nome" placeholder="Digite seu nome" />
                 <TextInput label="Cargo" placeholder="Digite seu cargo" />
                 <TextInput label="Imagem" placeholder="Digite o endereço da imagem" />
+                <DropdownList label="Teams" itens={teams} />
+                <Button>
+                    Create card
+                </Button>
             </form>
         </section>
     )
